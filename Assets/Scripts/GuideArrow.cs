5e1aeee3eb7ac7f695e94e461f4744f26abd052e
Ahmed Qaddora <hem-plo@hotmail.com>
2021-04-02 16:26:04 +0300

    add guide text

--- Assets/Scripts/GuideArrow.cs
+++ Assets/Scripts/GuideArrow.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GuideArrow: MonoBehaviour
 {
     public Transform targetObject;
-
+    public Text guideText;
     void Update()
     {
-        if(targetObject) transform.LookAt(targetObject);
+        if (targetObject)
+        { 
+            transform.LookAt(targetObject);
+            float dist = Vector3.Distance(transform.position, targetObject.position);
+            int distanceInMeters = Mathf.RoundToInt(dist);
+            if (dist > 0.7f)
+                guideText.text = "You are <B>" + distanceInMeters + "</B> meters away from your book";
+            else
+                guideText.text = "";
+        }
+        else
+        {
+            guideText.text = "verifying your book's location";
+        }
     }
 }
